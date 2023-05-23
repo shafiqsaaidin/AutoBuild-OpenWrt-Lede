a86a154fa0c1c4a2cfed4efec6bae49c2686534d
musha <mushagh@gmail.com>
2023-05-23 19:55:45 +0800

    fix typo

--- customize.sh
+++ customize.sh
@@ -8,4 +8,4 @@
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Modify xray core to v1.7.5.1
-sed -e 's/1.8.1/1.7.5.1/' -e 's/XTLS/mssvpn/' openwrt/package/feeds/packages/xray-core/Makefile
+sed -i -e 's/1.8.1/1.7.5.1/' -e 's/XTLS/mssvpn/' openwrt/package/feeds/packages/xray-core/Makefile
